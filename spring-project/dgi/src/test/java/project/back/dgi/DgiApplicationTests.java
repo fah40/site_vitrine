e3a435d553a87b4caa2653aefda4553fd58407b8
Tsiry23 <144609925+Tsiry23@users.noreply.github.com>
2025-02-20 14:28:42 +0300

    First commit

---
+++ spring-project/dgi/src/test/java/project/back/dgi/DgiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.back.dgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
