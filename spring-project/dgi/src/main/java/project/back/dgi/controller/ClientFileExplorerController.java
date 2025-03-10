aeb2d9928c03b7a92eb410503452fae678b57c45
Tsiry23 <144609925+Tsiry23@users.noreply.github.com>
2025-03-10 12:29:52 +0300

    another

---
+++ spring-project/dgi/src/main/java/project/back/dgi/controller/ClientFileExplorerController.java
@@ -0,0 +1,124 @@
+package project.back.dgi.controller;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletResponse;
+import project.back.dgi.entity.GeneralInfo;
+import project.back.dgi.entity.GeneralInfoValeur;
+import project.back.dgi.service.GeneralInfoService;
+import project.back.dgi.service.LangueService;
+
+@Controller
+public class ClientFileExplorerController {
+
+    @Autowired
+    private ServletContext servletContext;
+    @Autowired
+    private GeneralInfoService generalInfoService;
+    @Autowired
+    private LangueService langueService;
+
+
+    @Value("${app.storage.location}") // Injection du chemin défini dans application.properties
+    private String storagePath;
+
+    @Value("${file.upload-dir}")
+    private String uploadPath;
+
+    @GetMapping("/explorer")
+    public String listFiles(@RequestParam(required = false, defaultValue = "") String path,@RequestParam(required = false, defaultValue = "2") String langue , Model model, HttpServletResponse response) throws IOException {
+        File fileOrDirectory = new File(storagePath + File.separator + path);
+        System.out.println("Chemin utilisé : " + fileOrDirectory.getAbsolutePath());
+
+        if (!fileOrDirectory.exists()) {
+            throw new RuntimeException("Le chemin spécifié n'existe pas.");
+        }
+
+        // Si c'est un fichier, on déclenche le téléchargement
+        if (fileOrDirectory.isFile()) {
+            response.setContentType("application/octet-stream");
+            response.setHeader("Content-Disposition", "attachment; filename=\"" + fileOrDirectory.getName() + "\"");
+            Files.copy(fileOrDirectory.toPath(), response.getOutputStream());
+            response.getOutputStream().flush();
+            return null; // Empêche Thymeleaf de traiter une vue
+        }
+
+        // Si c'est un dossier, on affiche la liste des fichiers
+        File[] filesArray = fileOrDirectory.listFiles();
+        if (filesArray == null) {
+            throw new RuntimeException("Impossible de lister les fichiers dans le dossier.");
+        }
+
+        List<String> files = Arrays.stream(filesArray)
+                .map(File::getName)
+                .collect(Collectors.toList());
+
+        HashMap<String, GeneralInfo> generalInfoMap = new HashMap<>();
+        HashMap<String, GeneralInfo> generalInfoFolderMap = new HashMap<>();
+        String descri = path.startsWith("/") ? path : "/" + path;
+        GeneralInfoValeur giv = generalInfoService.getGeneralInfoValeurByKeyAndLanguage(generalInfoService.getByCle(descri).orElse(null), langueService.findById(Long.parseLong(langue)));
+        descri = giv == null ? "" : giv.getValeur();
+        String tempPath = new String(path);
+
+        for (String file : files) {
+            File relatedFile = new File(storagePath + File.separator + file);
+            
+            String key = '/' + file;
+            GeneralInfo generalInfo = generalInfoService.getByCle('/' + file).orElse(null);
+            if (!tempPath.isEmpty()) {
+                generalInfo = generalInfoService.getByCle(tempPath + '/' + file).orElse(null);
+                key = tempPath + '/' + file;
+            }
+
+            System.out.println("Clé : " + key);
+
+            if (generalInfo != null) {
+                System.out.println("file : " + file);
+
+                if (generalInfo.getLien().startsWith("/admin/")) {
+                    generalInfo.setLien(generalInfo.getLien().substring(generalInfo.getLien().indexOf("/explorer")));
+                }
+
+                generalInfoFolderMap.put(file, generalInfo);
+            } else {
+                if (!relatedFile.isDirectory()) {
+                    System.out.println("Is actually a file : " + file);
+
+                    generalInfoMap.put(file, generalInfo);
+                }
+            }
+        }
+
+        // au cas ou vide
+        if (files.isEmpty()) {
+            return "dossier-vide";
+        }
+
+        model.addAttribute("files", files);
+        model.addAttribute("mapFiles", generalInfoMap);
+        model.addAttribute("mapFolders", generalInfoFolderMap);
+        model.addAttribute("descri", descri);
+        model.addAttribute("currentPath", path);
+        model.addAttribute("displayPath", path);
+        model.addAttribute("mainGeneralInfo", giv);
+
+        System.out.println("Titre : " + "'" + giv.getTitre() + "'");
+
+        System.out.println("current path : " + path);
+
+        return "explorer-client"; // Retourne la vue Thymeleaf
+    }
+}
---
+++ spring-project/dgi/src/main/java/project/back/dgi/controller/ClientGetController.java
@@ -0,0 +1,50 @@
+package project.back.dgi.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import project.back.dgi.entity.Langue;
+import project.back.dgi.service.ActualiteService;
+import project.back.dgi.service.GeneralInfoService;
+import project.back.dgi.service.GeneralInfoValeurService;
+import project.back.dgi.service.LangueService;
+
+@Controller
+public class ClientGetController {
+    @Autowired
+    private LangueService langueService;
+    @Autowired
+    private GeneralInfoValeurService generalInfoValeurService;
+    @Autowired
+    private GeneralInfoService generalInfoService;
+    @Autowired
+    private ActualiteService actualiteService;
+
+    @GetMapping("/accueil")
+    public String accueil(@RequestParam(required = false, defaultValue = "2") String langue, Model model) {
+        List<Langue> langues = langueService.findAll();
+        long id_langue = Long.parseLong(langue);
+        model.addAttribute("langues", langues);
+        model.addAttribute("langueSelectionnee", id_langue);
+        model.addAttribute("a_propos", generalInfoValeurService.getGeneralInfoByKey("a_propos", id_langue));
+        model.addAttribute("dgi", generalInfoValeurService.getGeneralInfoByKey("dgi", id_langue));
+        model.addAttribute("mot_du_dgi", generalInfoValeurService.getGeneralInfoByKey("mot_du_dgi", id_langue));
+        model.addAttribute("legislation", generalInfoValeurService.getGeneralInfoByKey("legislation", id_langue));
+        model.addAttribute("ressources", generalInfoValeurService.getGeneralInfoByKey("ressources", id_langue));
+        model.addAttribute("analytiques_fiscales", generalInfoValeurService.getGeneralInfoByKey("analytiques_fiscales", id_langue));
+        model.addAttribute("historique", generalInfoValeurService.getGeneralInfoByKey("historique", id_langue));
+        model.addAttribute("vision", generalInfoValeurService.getGeneralInfoByKey("vision", id_langue));
+        model.addAttribute("attributions", generalInfoValeurService.getGeneralInfoByKey("attributions", id_langue));
+        model.addAttribute("e_service", generalInfoValeurService.getGeneralInfoByKey("e_service", id_langue));
+        model.addAttribute("votre_avis", generalInfoValeurService.getGeneralInfoByKey("votre_avis", id_langue));
+        model.addAttribute("centre_contact", generalInfoValeurService.getGeneralInfoByKey("centre_contact", id_langue));
+
+        model.addAttribute("allActualites", actualiteService.getAllActualites());
+
+        return "index-client";
+    }
+}
